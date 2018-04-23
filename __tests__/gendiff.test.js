debd09bc1bdd6263dddf12e7676c6015a451422a
Di Coding <dicoding@yandex.ru>
2018-04-23 22:54:49 +0300

    Step 2: create test for comparing flat JSONs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,10 @@
-test('test', () => {
-  expect(0).toBe(0);
+import fs from 'fs';
+import genDiff from '../src';
+
+it('Diff flat JSONs', () => {
+  const json1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/flat1.json', 'utf8'));
+  const json2 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/flat2.json', 'utf8'));
+  const actual = genDiff(json1, json2);
+  const expected = fs.readFileSync('./__tests__/__fixtures__/flatDiff.diff', 'utf8');
+  expect(actual).toBe(expected);
 });
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// it('Diff flat JSONs', () => {
+//   const json1 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/flat1.json', 'utf8'));
+//   const json2 = JSON.parse(fs.readFileSync('./__tests__/__fixtures__/flat2.json', 'utf8'));
+//   const actual = genDiff(json1, json2);
+//   const expected = fs.readFileSync('./__tests__/__fixtures__/flatDiff.diff', 'utf8');
+//   expect(actual).toBe(expected);
+// });
+const genDiff = (data1, data2) => {
+  
+};
+
+export default genDiff;
