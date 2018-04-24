d53f65ecf4ecc73d1c9b49f492d9a4358cf660fd
Di Coding <dicoding@yandex.ru>
2018-04-24 23:18:09 +0300

    Step 4: add INI support and optimize fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,13 +4,19 @@ import genDiff from '../src';
 describe('Tests', () => {
   it('Diff flat JSONs', () => {
     const actual = genDiff('./__tests__/__fixtures__/flat1.json', './__tests__/__fixtures__/flat2.json');
-    const expected = readFileSync('./__tests__/__fixtures__/flatJSON.diff', 'utf8');
+    const expected = readFileSync('./__tests__/__fixtures__/flat.diff', 'utf8');
     expect(actual).toBe(expected);
   });
 
   it('Diff flat YAMLs', () => {
     const actual = genDiff('./__tests__/__fixtures__/flat1.yml', './__tests__/__fixtures__/flat2.yml');
-    const expected = readFileSync('./__tests__/__fixtures__/flatYAML.diff', 'utf8');
+    const expected = readFileSync('./__tests__/__fixtures__/flat.diff', 'utf8');
+    expect(actual).toBe(expected);
+  });
+
+  it('Diff flat INIs', () => {
+    const actual = genDiff('./__tests__/__fixtures__/flat1.ini', './__tests__/__fixtures__/flat2.ini');
+    const expected = readFileSync('./__tests__/__fixtures__/flat.diff', 'utf8');
     expect(actual).toBe(expected);
   });
 
