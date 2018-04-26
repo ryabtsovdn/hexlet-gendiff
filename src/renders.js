fe54c6905a0e0aee0eb98497d09772ac5e237068
Di Coding <dicoding@yandex.ru>
2018-04-26 23:20:25 +0300

    optimize renders.js

--- src/renders.js
+++ src/renders.js
@@ -1,7 +1,9 @@
-import { flattenDeep } from 'lodash';
+import { flattenDeep, isObject } from 'lodash';
+
+const indent = size => '  '.repeat(size);
 
 const stringify = (value, deep) => {
-  if (typeof value !== 'object') {
+  if (!isObject(value)) {
     return value;
   }
   return JSON.stringify(value, null, 2)
@@ -11,38 +13,35 @@ const stringify = (value, deep) => {
         return str;
       }
       if (index === arr.length - 1) {
-        return `${'  '.repeat(deep + 1)}${str}`;
+        return `${indent(deep + 1)}${str}`;
       }
-      return `${'  '.repeat(deep + 2)}${str}`;
+      return `${indent(deep + 2)}${str}`;
     })
     .join('\n')
     .replace(/"/g, '');
 };
 
 const builders = {
-  unchanged: ({ name, value }, deep) => `${'  '.repeat(deep)}  ${name}: ${stringify(value, deep)}`,
-  added: ({ name, value }, deep) => `${'  '.repeat(deep)}+ ${name}: ${stringify(value, deep)}`,
-  deleted: ({ name, value }, deep) => `${'  '.repeat(deep)}- ${name}: ${stringify(value, deep)}`,
+  unchanged: ({ name, value }, deep) => `${indent(deep)}  ${name}: ${stringify(value, deep)}`,
+  added: ({ name, value }, deep) => `${indent(deep)}+ ${name}: ${stringify(value, deep)}`,
+  deleted: ({ name, value }, deep) => `${indent(deep)}- ${name}: ${stringify(value, deep)}`,
   changed: ({ name, value: [before, after] }, deep) => [
-    `${'  '.repeat(deep)}- ${name}: ${stringify(before, deep)}`,
-    `${'  '.repeat(deep)}+ ${name}: ${stringify(after, deep)}`,
+    `${indent(deep)}- ${name}: ${stringify(before, deep)}`,
+    `${indent(deep)}+ ${name}: ${stringify(after, deep)}`,
   ],
-  // eslint-disable-next-line no-use-before-define
-  merged: (node, deep) => build(node, deep + 1),
+  merged: (node, deep) => {
+    const { name, children } = node;
+    return [
+      deep === -1 ? '{' : `${indent(deep + 1)}${name}: {`,
+      children.map((child) => {
+        const build = builders[child.type];
+        return build(child, deep + 2);
+      }),
+      `${indent(deep + 1)}}`,
+    ];
+  },
 };
 
-const build = (ast, deep) => {
-  const { name, children } = ast;
-  return [
-    deep === 0 ? '{' : `${'  '.repeat(deep)}${name}: {`,
-    children.map((node) => {
-      const builder = builders[node.type];
-      return builder(node, deep + 1);
-    }),
-    `${'  '.repeat(deep)}}`,
-  ];
-};
-
-const render = ast => flattenDeep(build(ast, 0)).join('\n');
+const render = ast => flattenDeep(builders.merged(ast, -1)).join('\n');
 
 export default render;
