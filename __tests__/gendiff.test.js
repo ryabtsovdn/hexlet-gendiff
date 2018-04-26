33bfba5dde259bb622242b919b37ef0b418e0cb4
Di Coding <dicoding@yandex.ru>
2018-04-26 18:45:16 +0300

    Step 5: implement genAST function

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,29 +1,84 @@
 import { readFileSync } from 'fs';
-import genDiff from '../src';
+import { safeLoad } from 'js-yaml';
+import ini from 'ini';
+import { genDiff, genAST } from '../src';
 
-describe('Flat Files Tests', () => {
-  const expected = readFileSync('./__tests__/__fixtures__/flat.diff', 'utf8');
+describe('complex fixtures', () => {
+  it('have an appropriate content', () => {
+    const beforeJSON = readFileSync('./__tests__/__fixtures__/complex/before.json', 'utf8');
+    const afterJSON = readFileSync('./__tests__/__fixtures__/complex/after.json', 'utf8');
+    const beforeYAML = readFileSync('./__tests__/__fixtures__/complex/before.yml', 'utf8');
+    const afterYAML = readFileSync('./__tests__/__fixtures__/complex/after.yml', 'utf8');
+    const beforeINI = readFileSync('./__tests__/__fixtures__/complex/before.ini', 'utf8');
+    const afterINI = readFileSync('./__tests__/__fixtures__/complex/after.ini', 'utf8');
 
-  it('Diff flat JSONs', () => {
-    const actual = genDiff('./__tests__/__fixtures__/flat1.json', './__tests__/__fixtures__/flat2.json');
-    expect(actual).toBe(expected);
+    const expectedBefore = JSON.parse(beforeJSON);
+    const expectedAfter = JSON.parse(afterJSON);
+
+    expect(safeLoad(beforeYAML)).toEqual(expectedBefore);
+    expect(safeLoad(afterYAML)).toEqual(expectedAfter);
+    expect(ini.parse(beforeINI)).toEqual(expectedBefore);
+    expect(ini.parse(afterINI)).toEqual(expectedAfter);
+  });
+});
+
+describe('genAST', () => {
+  it('should form a correct AST', () => {
+    const astStr = readFileSync('./__tests__/__fixtures__/complex/ast.json', 'utf8');
+    const beforeJSON = readFileSync('./__tests__/__fixtures__/complex/before.json', 'utf8');
+    const afterJSON = readFileSync('./__tests__/__fixtures__/complex/after.json', 'utf8');
+
+    const expected = JSON.parse(astStr);
+    const actual = genAST(JSON.parse(beforeJSON), JSON.parse(afterJSON));
+
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('genDiff', () => {
+  const expectedFlat = readFileSync('./__tests__/__fixtures__/flat/flat.diff', 'utf8');
+  const expectedComplex = readFileSync('./__tests__/__fixtures__/complex/result.diff', 'utf8');
+
+  it('should compare flat JSONs', () => {
+    const actual = genDiff(
+      './__tests__/__fixtures__/flat/flat1.json',
+      './__tests__/__fixtures__/flat/flat2.json',
+    );
+    expect(actual).toBe(expectedFlat);
   });
 
-  it('Diff flat YAMLs', () => {
-    const actual = genDiff('./__tests__/__fixtures__/flat1.yml', './__tests__/__fixtures__/flat2.yml');
-    expect(actual).toBe(expected);
+  it('should compare flat YAMLs', () => {
+    const actual = genDiff(
+      './__tests__/__fixtures__/flat/flat1.yml',
+      './__tests__/__fixtures__/flat/flat2.yml',
+    );
+    expect(actual).toBe(expectedFlat);
   });
 
-  it('Diff flat INIs', () => {
-    const actual = genDiff('./__tests__/__fixtures__/flat1.ini', './__tests__/__fixtures__/flat2.ini');
-    expect(actual).toBe(expected);
+  it('should compare flat INIs', () => {
+    const actual = genDiff(
+      './__tests__/__fixtures__/flat/flat1.ini',
+      './__tests__/__fixtures__/flat/flat2.ini',
+    );
+    expect(actual).toBe(expectedFlat);
   });
 
-  it('Unsupported format', () => {
+  it('should throw unsupported format error', () => {
     try {
-      genDiff('./__tests__/__fixtures__/flat1.docx', './__tests__/__fixtures__/flat2.docx');
+      genDiff(
+        './__tests__/__fixtures__/flat/flat1.docx',
+        './__tests__/__fixtures__/flat/flat2.docx',
+      );
     } catch (error) {
       expect(error.message).toBe('File has unsupported format: .docx');
     }
+  });
+
+  it('should compare complex JSONs', () => {
+    const actual = genDiff(
+      './__tests__/__fixtures__/complex/before.json',
+      './__tests__/__fixtures__/complex/after.json',
+    );
+    expect(actual).toBe(expectedComplex);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 import { readFileSync } from 'fs';
 import { extname } from 'path';
-import { has, flatten } from 'lodash';
+import { has, flatten, isObject } from 'lodash';
 import getParser from './parsers';
 
-const genDiff = (path1, path2) => {
+const makeLeaf = (name, type, value) =>
+  ({ name, type, value });
+
+const makeBranch = (name, type, obj1, obj2) =>
+  ({
+    name,
+    type,
+    children: Object.keys({ ...obj1, ...obj2 })
+      .map((key) => {
+        if (!has(obj1, key)) {
+          return makeLeaf(key, 'added', obj2[key]);
+        }
+        if (!has(obj2, key)) {
+          return makeLeaf(key, 'deleted', obj1[key]);
+        }
+        if (isObject(obj1[key]) && isObject(obj2[key])) {
+          return makeBranch(key, 'merged', obj1[key], obj2[key]);
+        }
+        if (obj1[key] === obj2[key]) {
+          return makeLeaf(key, 'unchanged', obj1[key]);
+        }
+        return makeLeaf(key, 'changed', [obj1[key], obj2[key]]);
+      }),
+  });
+
+export const genAST = (obj1, obj2) =>
+  makeBranch('', 'merged', obj1, obj2);
+
+export const genDiff = (path1, path2) => {
   const ext1 = extname(path1);
   const data1 = readFileSync(path1, 'utf8');
   const obj1 = getParser(ext1)(data1);
