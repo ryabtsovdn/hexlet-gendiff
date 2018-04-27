2fed2e63785e5ebcd2bb5f6ab480fe6ae2405196
Di Coding <dicoding@yandex.ru>
2018-04-27 22:22:12 +0300

    Step 5: use polimorphism in AST generation process

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { readFileSync } from 'fs';
 import { extname } from 'path';
-import { has, isObject } from 'lodash';
+import { has, isObject, find } from 'lodash';
 import getParser from './parsers';
 import render from './renders';
 
-const makeLeaf = (name, type, oldValue, newValue) =>
+const makeNode = (name, type, oldValue, newValue) =>
   ({
     name,
     type,
@@ -12,28 +12,42 @@ const makeLeaf = (name, type, oldValue, newValue) =>
     newValue,
   });
 
-const makeBranch = (name, type, obj1, obj2) => {
-  const children = Object.keys({ ...obj1, ...obj2 })
-    .map((key) => {
-      if (!has(obj1, key)) {
-        return makeLeaf(key, 'added', '', obj2[key]);
-      }
-      if (!has(obj2, key)) {
-        return makeLeaf(key, 'deleted', obj1[key], '');
-      }
-      if (isObject(obj1[key]) && isObject(obj2[key])) {
-        return makeBranch(key, 'merged', obj1[key], obj2[key]);
-      }
-      if (obj1[key] === obj2[key]) {
-        return makeLeaf(key, 'unchanged', obj1[key], obj1[key]);
-      }
-      return makeLeaf(key, 'changed', obj1[key], obj2[key]);
-    });
-  return { name, type, children };
-};
+const diffTypes = [
+  {
+    type: 'added',
+    check: (obj1, obj2, key) => !has(obj1, key),
+    process: (obj1, obj2, key) => makeNode(key, 'added', '', obj2[key]),
+  },
+  {
+    type: 'deleted',
+    check: (obj1, obj2, key) => !has(obj2, key),
+    process: (obj1, obj2, key) => makeNode(key, 'deleted', obj1[key], ''),
+  },
+  {
+    type: 'merged',
+    check: (obj1, obj2, key) => isObject(obj1[key]) && isObject(obj2[key]),
+    process: (obj1, obj2, key, cb) => cb(obj1[key], obj2[key], key),
+  },
+  {
+    type: 'unchanged',
+    check: (obj1, obj2, key) => obj1[key] === obj2[key],
+    process: (obj1, obj2, key) => makeNode(key, 'unchanged', obj1[key], obj2[key]),
+  },
+  {
+    type: 'changed',
+    check: (obj1, obj2, key) => obj1[key] !== obj2[key],
+    process: (obj1, obj2, key) => makeNode(key, 'changed', obj1[key], obj2[key]),
+  },
+];
 
-export const genAST = (obj1, obj2) =>
-  makeBranch('', 'merged', obj1, obj2);
+export const genAST = (obj1, obj2, name = '') => {
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  const children = keys.map((key) => {
+    const { process } = find(diffTypes, ({ check }) => check(obj1, obj2, key));
+    return process(obj1, obj2, key, genAST);
+  });
+  return { name, type: 'merged', children };
+};
 
 const genDiff = (path1, path2) => {
   const ext1 = extname(path1);
