d0cae172e94210eedb1bea182a3894a3e7cd2f70
Di Coding <dicoding@yandex.ru>
2018-04-27 18:26:46 +0300

    Step 5: delete excess fixtures tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,26 +1,5 @@
 import { readFileSync } from 'fs';
-import { safeLoad } from 'js-yaml';
-import ini from 'ini';
 import genDiff, { genAST } from '../src';
-
-describe('complex fixtures', () => {
-  it('have an appropriate content', () => {
-    const beforeJSON = readFileSync('./__tests__/__fixtures__/complex/before.json', 'utf8');
-    const afterJSON = readFileSync('./__tests__/__fixtures__/complex/after.json', 'utf8');
-    const beforeYAML = readFileSync('./__tests__/__fixtures__/complex/before.yml', 'utf8');
-    const afterYAML = readFileSync('./__tests__/__fixtures__/complex/after.yml', 'utf8');
-    const beforeINI = readFileSync('./__tests__/__fixtures__/complex/before.ini', 'utf8');
-    const afterINI = readFileSync('./__tests__/__fixtures__/complex/after.ini', 'utf8');
-
-    const expectedBefore = JSON.parse(beforeJSON);
-    const expectedAfter = JSON.parse(afterJSON);
-
-    expect(safeLoad(beforeYAML)).toEqual(expectedBefore);
-    expect(safeLoad(afterYAML)).toEqual(expectedAfter);
-    expect(ini.parse(beforeINI)).toEqual(expectedBefore);
-    expect(ini.parse(afterINI)).toEqual(expectedAfter);
-  });
-});
 
 describe('genAST', () => {
   it('should form a correct AST', () => {
